c0ca7755f0621f30831d2b2b9d1ecb0a72bc7f65
Lawand Othman <lwnd@pm.me>
2021-05-31 23:42:40 +0100

    set up dark mode support

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,12 @@
-import { extendTheme } from '@chakra-ui/react'
+import { extendTheme, ThemeConfig } from '@chakra-ui/react'
+
+const config: ThemeConfig = {
+  initialColorMode: 'light',
+  useSystemColorMode: true,
+}
 
 const theme = extendTheme({
+  ...config,
   fonts: {
     body: 'Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
   },
