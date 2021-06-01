94052df7db78b4e854857bfa61a69f336b3e1694
Lawand Othman <lwnd@pm.me>
2021-06-01 13:30:30 +0100

    index with introduction content

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,34 @@
-import { Heading } from '@chakra-ui/react'
+import {
+  Box, Heading, Text, useColorModeValue,
+} from '@chakra-ui/react'
 import Head from 'next/head'
+import Shell from './components/Shell'
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Create Next App</title>
-      <link rel='icon' href='/favicon.ico' />
-    </Head>
+const Home = () => {
+  const color = useColorModeValue('blackAlpha.900', 'whiteAlpha.900')
+  return (
+    <div>
+      <Head>
+        <title>lwnd.me</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-    <main>
-      <Heading>
-        Hey, I&apos;m Lawand Othman
-      </Heading>
-    </main>
-  </div>
-)
+      <Shell>
+        <Box ml='auto' mr='auto' maxWidth={590} mt={20}>
+          <Heading color={color} mb={4}>
+            Hey, I’m Lawand Othman
+          </Heading>
+          <Text color={color} pb={2} fontWeight='medium'>
+            I’m a software devleoper based in Birmingham, UK.
+          </Text>
+          <Text color={color} fontWeight='medium'>
+            You’ve found my personal slice of the internet where I share all the
+            beautiful and fun stuff I’m working on.
+          </Text>
+        </Box>
+      </Shell>
 
+    </div>
+  )
+}
 export default Home
