ca43e43c8ceb389ad4a6181de6ba59bf643f4702
Lawand Othman <lwnd@pm.me>
2021-06-01 14:43:54 +0100

    secondary white / gray color for text

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,13 @@ import {
   Box, Heading, Text, useColorModeValue,
 } from '@chakra-ui/react'
 import Head from 'next/head'
+import React from 'react'
 import Shell from './components/Shell'
 
-const Home = () => {
-  const color = useColorModeValue('blackAlpha.900', 'whiteAlpha.900')
+const Home:React.FC = () => {
+  const headingColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.900')
+  const textColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.700')
+
   return (
     <div>
       <Head>
@@ -15,19 +18,18 @@ const Home = () => {
 
       <Shell>
         <Box ml='auto' mr='auto' maxWidth={590} mt={20}>
-          <Heading color={color} mb={4}>
+          <Heading color={headingColor} mb={4}>
             Hey, I’m Lawand Othman
           </Heading>
-          <Text color={color} pb={2} fontWeight='medium'>
+          <Text color={textColor} pb={2} fontWeight='medium'>
             I’m a software devleoper based in Birmingham, UK.
           </Text>
-          <Text color={color} fontWeight='medium'>
+          <Text color={textColor} fontWeight='medium'>
             You’ve found my personal slice of the internet where I share all the
             beautiful and fun stuff I’m working on.
           </Text>
         </Box>
       </Shell>
-
     </div>
   )
 }
