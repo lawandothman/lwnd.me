f792122eb872c0970ba7a0d41215773f9870f1f7
Lawand Othman <lwnd@pm.me>
2021-06-01 16:57:35 +0100

    add title tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,6 +19,7 @@ class MyDocument extends Document {
       <Html lang='en'>
         <Head>
           <link rel='icon' href='/favicon.ico' />
+          <title>lwnd.me</title>
           <meta name='viewport' content='initial-scale=1.0, width=device-width' />
           <meta name='description' content='Lawand Othman&apos;s Personal Site' />
         </Head>
