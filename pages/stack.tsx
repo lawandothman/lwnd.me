efe5a7acd5c668231259e5bb952fa5303ccd64e7
Lawand Othman <lwnd@pm.me>
2021-06-02 18:22:25 +0100

    stack page

---
+++ pages/stack.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {
+  Box, Heading, Text, useColorModeValue,
+} from '@chakra-ui/react'
+import Shell from '@/components/Shell'
+import Head from 'next/head'
+
+const Stack: React.FC = () => {
+  const headingColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.900')
+  const textColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.700')
+
+  return (
+    <div>
+      <Head>
+        <title>lwnd</title>
+      </Head>
+      <Shell>
+        <Box mt={20}>
+          <Heading color={headingColor} mb={4}>
+            Stack
+          </Heading>
+          <Text color={textColor} mb={2} fontWeight='medium'>
+            My favourite tools and software.
+          </Text>
+        </Box>
+      </Shell>
+    </div>
+  )
+}
+export default Stack
