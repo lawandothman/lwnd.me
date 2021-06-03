0833e00eee71735aeaa810030f71b5402ebccdff
Lawand Othman <lwnd@pm.me>
2021-06-03 13:33:20 +0100

    add first project

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import {
-  Box, Heading, Text, useColorModeValue,
+  Badge,
+  Box,
+  Heading,
+  ListItem,
+  Text,
+  UnorderedList,
+  useColorModeValue,
+  VStack,
 } from '@chakra-ui/react'
 import Shell from '@/components/Shell'
 import Head from 'next/head'
+import NextChakraLink from '@/components/NextChakraLink'
 
-const Projects:React.FC = () => {
+const Projects: React.FC = () => {
   const headingColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.900')
   const textColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.700')
+  const badgeColor = useColorModeValue('blackAlpha', 'whiteAlpha')
 
   return (
     <div>
@@ -19,9 +28,34 @@ const Projects:React.FC = () => {
           <Heading color={headingColor} mb={4}>
             Projects
           </Heading>
-          <Text color={textColor} mb={2} fontSize='sm'>
-            This page is under construction, please check back later.
-          </Text>
+          <UnorderedList ml={8} listStyleType='none'>
+            <VStack spacing={4} align='stretch'>
+              <ListItem fontWeight='bold'>
+                <NextChakraLink
+                  href='https://github.com/lawandothman/pokedex'
+                  _hover={{ textDecoration: 'none' }}
+                  target='blank'
+                  _focus={{ boxShadow: 'none' }}
+                >
+                  Pokédex
+                </NextChakraLink>
+                <Badge
+                  variant='outline'
+                  ml={2}
+                  px={2}
+                  colorScheme={badgeColor}
+                  as='a'
+                  href='https://law-pokedex.herokuapp.com/'
+                  target='_blank'
+                >
+                  Live
+                </Badge>
+                <Text fontWeight='light' color={textColor}>
+                  My first project in my journey to learn web development
+                </Text>
+              </ListItem>
+            </VStack>
+          </UnorderedList>
         </Box>
       </Shell>
     </div>
