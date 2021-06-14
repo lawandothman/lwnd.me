8f4798a263476134324d74d1ce701f58ead48f43
Lawand Othman <lwnd@pm.me>
2021-06-14 17:19:21 +0100

    add more tools

--- data/stack.ts
+++ data/stack.ts
@@ -1,4 +1,6 @@
-import { Figma, NextJs, VSCode } from '@/components/Icons'
+import {
+  Dependabot, Figma, NextJs, Notion, Vercel, VSCode,
+} from '@/components/Icons'
 import { IStack } from 'types'
 
 const stack: IStack[] = [
@@ -20,6 +22,25 @@ const stack: IStack[] = [
     url: 'https://www.figma.com/',
     icon: Figma({ viewBox: '0 0 39 58', w: '58', height: '58' }),
   },
+  {
+    name: 'Vercel',
+    description: 'Everything I build on the internet lives on Vercel. It is the gold standard of developer tools',
+    url: 'https://vercel.com/',
+    icon: Vercel({ viewBox: '0 0 58 58', w: '58', height: '58' }),
+  },
+  {
+    name: 'Dependabot',
+    description: 'Dependabot keeps all of my project dependecies up to date every month',
+    url: 'https://dependabot.com/',
+    icon: Dependabot({ viewBox: '0 0 58 58', w: '58', height: '58' }),
+  },
+  {
+    name: 'Notion',
+    description: 'My favourite productivity tool to keep notes and manage my tasks',
+    url: 'https://www.notion.so/',
+    icon: Notion({ viewBox: '0 0 58 58', w: '58', height: '58' }),
+  },
+
 ]
 
 export default stack
