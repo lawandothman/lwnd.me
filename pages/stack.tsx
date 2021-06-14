6f1fc91c38222764af095421c33e23430756ecf8
Lawand Othman <lwnd@pm.me>
2021-06-14 12:26:57 +0100

    stack page init

--- pages/stack.tsx
+++ pages/stack.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import { Heading, Text, useColorModeValue } from '@chakra-ui/react'
+import {
+  Heading,
+  Text,
+  useColorModeValue,
+  VStack,
+  Flex,
+} from '@chakra-ui/react'
+import { VSCode } from '@/components/Icons'
 import Shell from '@/components/Shell'
 import Head from 'next/head'
 
 const Stack: React.FC = () => {
   const headingColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.900')
   const textColor = useColorModeValue('blackAlpha.900', 'whiteAlpha.700')
+  const backgroundColor = useColorModeValue('gray.100', 'whiteAlpha.200')
 
   return (
     <div>
@@ -16,12 +24,30 @@ const Stack: React.FC = () => {
         <Heading color={headingColor} mb={4}>
           Stack
         </Heading>
-        <Text color={textColor} mb={2} fontWeight='medium'>
+        <Text color={textColor} mb={8} fontWeight='medium'>
           My favourite tools and software.
         </Text>
-        <Text color={textColor} mb={2} fontSize='sm'>
-          This page is under construction, please check back later.
-        </Text>
+        <VStack spacing={4}>
+          <Flex
+            as='a'
+            href='https://code.visualstudio.com/'
+            target='_blank'
+            alignItems='center'
+            width='full'
+            borderRadius={8}
+            p={2}
+            transitionDuration='0.25s'
+            _hover={{ backgroundColor }}
+          >
+            <VSCode viewBox='0 0 58 58' w='58' height='58' />
+            <Flex direction='column' ml={6}>
+              <Text fontWeight='medium'>VSCode</Text>
+              <Text fontSize='sm' fontWeight='light' color={textColor}>
+                My code editor of choice
+              </Text>
+            </Flex>
+          </Flex>
+        </VStack>
       </Shell>
     </div>
   )
