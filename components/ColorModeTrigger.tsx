98ccf3db5dab5b9ea290f7fea340d38a5c25ec44
Lawand Othman <lwnd@pm.me>
2022-02-24 00:21:50 +0000

    Refactor: cleanup ColorModeTrigger

--- components/ColorModeTrigger.tsx
+++ components/ColorModeTrigger.tsx
@@ -1,26 +1,22 @@
 import { MoonIcon, SunIcon } from '@chakra-ui/icons'
-import { Flex, IconButton, useColorMode } from '@chakra-ui/react'
+import { IconButton, useColorMode } from '@chakra-ui/react'
 
 const ColorModeTrigger = () => {
   const { colorMode, toggleColorMode } = useColorMode()
+
   return (
-    <Flex mr={4}>
-      {colorMode === 'light' ? (
-        <IconButton
-          onClick={toggleColorMode}
-          _focus={{ boxShadow: 'none' }}
-          aria-label='Dark Mode'
-          icon={<MoonIcon color='icon' />}
-        />
-      ) : (
-        <IconButton
-          onClick={toggleColorMode}
-          _focus={{ boxShadow: 'none' }}
-          aria-label='Light Mode'
-          icon={<SunIcon color='icon' />}
-        />
-      )}
-    </Flex>
+    <IconButton
+      onClick={toggleColorMode}
+      _focus={{ boxShadow: 'none' }}
+      aria-label={colorMode === 'light' ? 'Dark Mode' : 'Light Mode'}
+      icon={
+        colorMode === 'light' ? (
+          <MoonIcon color='icon' />
+        ) : (
+          <SunIcon color='icon' />
+        )
+      }
+    />
   )
 }
 
