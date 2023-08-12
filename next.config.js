3145de3f64fb1192c0b9a105c49ef73456ed1699
Lawand Othman <lwnd@pm.me>
2023-08-12 11:53:38 +0100

    Initial commit from Create Next App

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-// @ts-check
-
-/**
- * @type {import('next').NextConfig}
- **/
-const nextConfig = {
-	reactStrictMode: true,
-	swcMinify: true,
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
 
 module.exports = nextConfig
