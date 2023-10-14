d35c64558e96b1bb49377d77451038c01faea2ef
Lawand Othman <lwnd@pm.me>
2023-10-14 17:45:06 +0100

    lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-	return <h1 className='text-3xl font-semibold'>Hey, I&apos;m Lawand Othman</h1>
+	return (
+		<h1 className='text-3xl font-semibold'>Hey, I&apos;m Lawand Othman</h1>
+	)
 }
