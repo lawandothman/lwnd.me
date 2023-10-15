6e21de23b2c289803c3036d35cb135cf3935463b
Lawand Othman <lwnd@pm.me>
2023-10-15 20:24:14 +0100

    testing something

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
 			>
 				<ThemeProvider
 					attribute='class'
-					defaultTheme='dark'
+					defaultTheme='system'
 					enableSystem
 					disableTransitionOnChange
 				>
