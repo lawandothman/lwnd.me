a0ccbaaebf0afdc67f1f6371e9b4a1693006d763
Lawand Othman <lwnd@pm.me>
2023-10-15 21:36:15 +0100

    bun biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-import React from 'react'
-
 export default function Home() {
-	return (
-		<React.Fragment>
-			<h1 className='text-3xl font-semibold mb-8'>
-				Hey, I&apos;m Lawand 👋
-			</h1>
-			<p className='text-lg mb-8'>
-				I&apos;m a software engineer based in London 🇬🇧
-			</p>
-			<p>
-				You&apos;ve found my personal slice of the internet where I
-				share all the beautiful and fun stuff I&apos;m working on.
-			</p>
-		</React.Fragment>
-	)
+	return <div>Hello world</div>;
 }
