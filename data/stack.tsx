7421f15970d4d48192af2c817d251927020d5e1e
Lawand Othman <lwnd@pm.me>
2024-10-04 21:25:56 +0100

    update stack

--- data/stack.tsx
+++ data/stack.tsx
@@ -1,6 +1,6 @@
 import { IStack } from 'types'
 
-import { GraphQL, Rust, Typescript } from 'components'
+import { GraphQL, Kitty, Neovim, Raycast, Rust, Typescript } from 'components'
 
 const stack: IStack[] = [
 	{
@@ -21,6 +21,24 @@ const stack: IStack[] = [
 		url: 'https://graphql.org/',
 		icon: <GraphQL viewBox='0 4 395 395' width='58' height='58' />,
 	},
+	{
+		name: 'Neovim',
+		description: '\"I use Vim by the way!\"',
+		url: 'https://neovim.io/',
+		icon: <Neovim viewBox='10 0 602 734' width='58' height='58' />,
+	},
+	{
+		name: 'Kitty',
+		description: 'Fast, featureful, GPU based terminal emulator',
+		url: 'https://sw.kovidgoyal.net/kitty/',
+		icon: <Kitty viewBox='0 0 240 240' width='58' height='58' />,
+	},
+	{
+		name: 'Raycast',
+		description: 'My shortcut to everything',
+		url: 'https://raycast.com/',
+		icon: <Raycast viewBox='0 0 40 40' width='58' height='58' />,
+	}
 ]
 
 export default stack
