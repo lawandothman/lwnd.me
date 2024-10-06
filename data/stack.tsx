276bfc3c52990828119034aa40948ad020fb031e
Lawand Othman <lwnd@pm.me>
2024-10-06 11:18:20 +0100

    rust on top

--- data/stack.tsx
+++ data/stack.tsx
@@ -4,16 +4,16 @@ import { GraphQL, Kitty, Neovim, Raycast, Rust, Typescript } from 'components'
 
 const stack: IStack[] = [
 	{
-		name: 'TypeScript',
-		description: 'JavaScript but with no surprises',
-		url: 'https://www.typescriptlang.org/',
-		icon: <Typescript viewBox='-20 0 550 550' width='58' height='58' />,
-	},
-	{
 		name: 'Rust',
 		description: 'Blazingly fast',
 		url: 'https://www.rust-lang.org/',
 		icon: <Rust viewBox='3 3 100 100' width='58' height='58' fill='icon' />,
+	},
+	{
+		name: 'TypeScript',
+		description: 'JavaScript but with no surprises',
+		url: 'https://www.typescriptlang.org/',
+		icon: <Typescript viewBox='-20 0 550 550' width='58' height='58' />,
 	},
 	{
 		name: 'GraphQL',
