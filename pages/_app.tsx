aa8eaf67ca9ec0a56c5adbb07eb015f626045139
Lawand Othman <lwnd@pm.me>
2024-10-08 16:38:09 +0100

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { DefaultSeo } from 'next-seo'
 import SEO from 'next-seo.config'
 import { AppProps } from 'next/app'
+import { Analytics } from '@vercel/analytics/react'
 
 import theme from 'styles/theme'
 
@@ -11,6 +12,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 			<DefaultSeo {...SEO} />
 			<ChakraProvider theme={theme} resetCSS>
 				<Component {...pageProps} />
+				<Analytics />
 			</ChakraProvider>
 		</>
 	)
